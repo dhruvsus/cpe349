7e00b6ee29e0eff382f858d25489fd5a9d346aa7
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-01-13 14:31:47 -0800

    todo: asgn1

--- lab1/SubsetGen.java
+++ lab1/SubsetGen.java
@@ -4,52 +4,55 @@ import java.util.ArrayList;
 
 public class SubsetGen {
 
-    public SubsetGen() {}
+	//empty constructor
+	public SubsetGen() {}
 
-    public ArrayList < String > getSubsets(String word) {
-        ArrayList < String >
-            subsets = new ArrayList < String > (); //   Put your algorithm here
-        ArrayList < String > temp = new ArrayList < String > ();
-        if (word.length() > 0) {
-            temp = getSubsets(word.substring(0, word.length() - 1));
-            String snippedCharacter = word.substring(word.length()-1);
-            for (int i = 0; i < temp.size(); i++) {
-                subsets.add(temp.get(i));
-            }
-            for (int i = 0; i < temp.size(); i++) {
-                subsets.add(temp.get(i) + snippedCharacter);
-            }
-            return subsets;
-        } else {
-            subsets.add("");
-            return subsets;
-        }
-    }
-    public static void main(String[] args)   {
-      SubsetGen generator = new SubsetGen();
-      ArrayList<String> subsets = generator.getSubsets("abc");
-      System.out.println(subsets);
-      System.out.println(generator.getGrayCode(3));
-    }
-    public ArrayList<String> getGrayCode(int n){
-      ArrayList<String> grayCode = new ArrayList<String>();
-      ArrayList<String> temp = new ArrayList<String>();
-      if(n>1){
-        temp=getGrayCode(--n);
-        //prepend a 0
-        for(int i=0;i<temp.size();i++){
-          grayCode.add("0"+temp.get(i));
-        }
-        //prepend 1
-        for(int i=temp.size();i-- > 0;){
-          grayCode.add("1"+temp.get(i));
-        }
-        return grayCode;
-      }
-      else{
-        grayCode.add("0");
-        grayCode.add("1");
-        return grayCode;
-      }
-    }
+	public ArrayList < String > getSubsets(String word) {
+		ArrayList < String >
+			subsets = new ArrayList < String > (); //   Put your algorithm here
+		ArrayList < String > temp = new ArrayList < String > ();
+		if (word.length() > 0) {
+			temp = getSubsets(word.substring(0, word.length() - 1));
+			String snippedCharacter = word.substring(word.length()-1);
+			//loop over temp and create subsets with and without the
+			//last character of the original string
+			for (int i = 0; i < temp.size(); i++) {
+				subsets.add(temp.get(i)); //add subsets without the last character
+			}
+			for (int i = 0; i < temp.size(); i++) {
+				subsets.add(temp.get(i) + snippedCharacter);
+				//add subsets with last character
+			}
+			return subsets;
+		} else {
+			//the empty set is the only subset of the empty set
+			subsets.add("");
+			//empty arraylist
+			return subsets;
+		}
+	}
+
+	public ArrayList<String> getGrayCode(int n){
+		ArrayList<String> grayCode = new ArrayList<String>();
+		ArrayList<String> temp = new ArrayList<String>();
+		if(n>1){
+			//recursive
+			temp=getGrayCode(--n);
+			//prepend a 0
+			for(int i=0;i<temp.size();i++){
+				grayCode.add("0"+temp.get(i));
+			}
+			//prepend 1
+			for(int i=temp.size();i-- > 0;){
+				grayCode.add("1"+temp.get(i));
+			}
+			return grayCode;
+		}
+		else{
+			//graycode for n=1 is just {0,1}, it's the base case
+			grayCode.add("0");
+			grayCode.add("1");
+			return grayCode;
+		}
+	}
 }
--- lab1/SubsetGenTest.java
+++ lab1/SubsetGenTest.java
@@ -1,36 +1,38 @@
 import java.util.ArrayList;
 
 /**
-   This program demonstrates the permutation generator.
-*/
+  This program demonstrates the permutation generator.
+  */
 public class SubsetGenTest
 {
-   public static void main(String[] args)   {
+	public static void main(String[] args)   {
 
-      // SubsetGen generator = new SubsetGen();
+		// SubsetGen generator = new SubsetGen();
 
-     ArrayList<String> answer = new ArrayList<String>();
-     answer.add("");
-     answer.add("a");
-     answer.add("b");
-     answer.add("ab");
-     answer.add("c");
-     answer.add("ac");
-     answer.add("bc");
-     answer.add("abc");  
+		ArrayList<String> answer = new ArrayList<String>();
+		answer.add("");
+		answer.add("a");
+		answer.add("b");
+		answer.add("ab");
+		answer.add("c");
+		answer.add("ac");
+		answer.add("bc");
+		answer.add("abc");  
 
-     SubsetGen generator = new SubsetGen();
-     ArrayList<String> subsets = generator.getSubsets("abc");
-      boolean match = true;
-      int i = 0;
+		SubsetGen generator = new SubsetGen();
+		ArrayList<String> subsets = generator.getSubsets("abc");
+		ArrayList<String> grayCode=generator.getGrayCode(4); 
+		boolean match = true;
+		int i = 0;
 		while ((match == true)&&(i<8))   {
-         if (!answer.get(i).equals(subsets.get(i)))
-            match = false;
-         i++;
-      }
-      if (match == true)
-         System.out.println("Congratulations all the strings match." );
-      else
-         System.out.println("your string at position  " + i  + " should be -" + answer.get(i-1)+ "- you returned -" + subsets.get(i-1)+"-");
-   }
+			if (!answer.get(i).equals(subsets.get(i)))
+				match = false;
+			i++;
+		}
+		if (match == true)
+			System.out.println("Congratulations all the strings match." );
+		else
+			System.out.println("your string at position  " + i  + " should be -" + answer.get(i-1)+ "- you returned -" + subsets.get(i-1)+"-");
+		System.out.println(grayCode);
+	}
 }
