9cdc1fc6916a17851d188ffdc46edb61550f9d7d
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-01-16 23:26:22 +0000

    beautified code. now to check if it still compiles and works

--- lab2/CombObjects.java
+++ lab2/CombObjects.java
@@ -1,68 +1,68 @@
 import java.util.ArrayList;
 
-public class CombObjects{
-	public CombObjects(){}
+public class CombObjects {
+ public CombObjects() {}
 
-	public static ArrayList<String> getLexPerm(String str){
-		ArrayList<String> perm=new ArrayList<String>();
-		ArrayList<String> temp=new ArrayList<String>();
+ public static ArrayList < String > getLexPerm(String str) {
+  ArrayList < String > perm = new ArrayList < String > ();
+  ArrayList < String > temp = new ArrayList < String > ();
 
-		// base case if length of string is <=1
-		if(str.length()<=1){
-			perm.add(str);
-			return perm;
-		}
-		for(int i=0;i<str.length();i++){
-			//remove the character
-			char removed=str.charAt(i);
-			String tempStr=new String(str);
-			tempStr=str.replace(Character.toString(removed),"");
-			temp=getLexPerm(tempStr);
-			//generate recursive permutations
-			//by adding the removed character to the front
-			for(int j=0;j<temp.size();j++){
-				perm.add(removed+temp.get(j));
-			}
-		}
-		return perm;
-	}
+  // base case if length of string is <=1
+  if (str.length() <= 1) {
+   perm.add(str);
+   return perm;
+  }
+  for (int i = 0; i < str.length(); i++) {
+   //remove the character
+   char removed = str.charAt(i);
+   String tempStr = new String(str);
+   tempStr = str.replace(Character.toString(removed), "");
+   temp = getLexPerm(tempStr);
+   //generate recursive permutations
+   //by adding the removed character to the front
+   for (int j = 0; j < temp.size(); j++) {
+    perm.add(removed + temp.get(j));
+   }
+  }
+  return perm;
+ }
 
-	public static ArrayList<String> getMinChgPerm(String str){
-		ArrayList<String> perm=new ArrayList<String>();
-		ArrayList<String> temp=new ArrayList<String>();
-		if(str.isEmpty()){
-			perm.add("");
-			return perm;
-		}
-		//remove the last character
-		char removed=str.charAt(str.length()-1);
-		//debugging, print removed character and resulting string
-		//System.out.println(removed);
-		String tempStr=new String(str);
-		tempStr=str.replace(Character.toString(removed),"");
-		//System.out.println(tempStr);
+ public static ArrayList < String > getMinChgPerm(String str) {
+  ArrayList < String > perm = new ArrayList < String > ();
+  ArrayList < String > temp = new ArrayList < String > ();
+  if (str.isEmpty()) {
+   perm.add("");
+   return perm;
+  }
+  //remove the last character
+  char removed = str.charAt(str.length() - 1);
+  //debugging, print removed character and resulting string
+  //System.out.println(removed);
+  String tempStr = new String(str);
+  tempStr = str.replace(Character.toString(removed), "");
+  //System.out.println(tempStr);
 
-		//recursively generate permuatations without removed character
-		temp=getMinChgPerm(tempStr);
-		for(int i=0;i<temp.size();i++){
-			//if i is even add removed character right to left
-			StringBuilder sb = new StringBuilder(temp.get(i));
-			if(i%2==0){
-				for(int j=temp.get(i).length();j>=0;j--){
-					perm.add(temp.get(i).substring(0, j) + removed + temp.get(i).substring(j));
-				}
-			}
-			//if i is odd then add character left to right
-			else{
-				for(int j=0;j<=temp.get(i).length();j++){
-					perm.add(temp.get(i).substring(0,j)+removed+temp.get(i).substring(j));
-				}
-			}
-			//System.out.println(perm);
-		}
-		return perm;
-	}
-	/*
+  //recursively generate permuatations without removed character
+  temp = getMinChgPerm(tempStr);
+  for (int i = 0; i < temp.size(); i++) {
+   //if i is even add removed character right to left
+   StringBuilder sb = new StringBuilder(temp.get(i));
+   if (i % 2 == 0) {
+    for (int j = temp.get(i).length(); j >= 0; j--) {
+     perm.add(temp.get(i).substring(0, j) + removed + temp.get(i).substring(j));
+    }
+   }
+   //if i is odd then add character left to right
+   else {
+    for (int j = 0; j <= temp.get(i).length(); j++) {
+     perm.add(temp.get(i).substring(0, j) + removed + temp.get(i).substring(j));
+    }
+   }
+   //System.out.println(perm);
+  }
+  return perm;
+ }
+ /*
 	   public static void main(String[] args){
 //driver program
 ArrayList<String> perm = new ArrayList<String>();
