856bdeed46bcc164994ef4f2b172ca84cb5881ac
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-01-17 10:20:36 -0800

    exploring the BFS code. todo: implement bfs and read more

--- lab2/MyGraph.java
+++ lab2/MyGraph.java
@@ -6,50 +6,50 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Collections;
 
-class GraphStart3  {	
+class MyGraph  {	
 	ArrayList<Vertex> vertices = new ArrayList<Vertex>();
-   boolean directed; 
-   int nvertices;
-   int nedges; 
-   int numComp;
-	
-   public GraphStart3(){
-   }
-	
-   void readfile_graph(String filename) throws FileNotFoundException  {
-      int x,y;
-        //read the input
-      FileInputStream in = new FileInputStream(new File(filename));
-      Scanner sc = new Scanner(in);
-      int temp = sc.nextInt(); // if 1 directed; 0 undirected
-      directed = (temp == 1);
-    	nvertices = sc.nextInt();
-      for (int i=0; i<=nvertices-1; i++){
-         Vertex tempv = new Vertex(i+1);   // kludge to store proper key starting at 1
-         vertices.add(tempv);
-      }
+	boolean directed; 
+	int nvertices;
+	int nedges; 
+	int numComp;
+
+	public MyGraph(){
+	}
+
+	void readfile_graph(String filename) throws FileNotFoundException  {
+		int x,y;
+		//read the input
+		FileInputStream in = new FileInputStream(new File(filename));
+		Scanner sc = new Scanner(in);
+		int temp = sc.nextInt(); // if 1 directed; 0 undirected
+		directed = (temp == 1);
+		nvertices = sc.nextInt();
+		for (int i=0; i<=nvertices-1; i++){
+			Vertex tempv = new Vertex(i+1);   // kludge to store proper key starting at 1
+			vertices.add(tempv);
+		}
 
 		nedges = sc.nextInt();   // m is the number of edges in the file
-      int nedgesFile = nedges;
+		int nedgesFile = nedges;
 		for (int i=1; i<=nedgesFile ;i++)	{
-			// System.out.println(i + " compare " + (i<=nedges) + " nedges " + nedges);
-         x=sc.nextInt();
+			System.out.println(i + " compare " + (i<=nedges) + " nedges " + nedges);
+			x=sc.nextInt();
 			y=sc.nextInt();
-         //  System.out.println("x  " + x + "  y:  " + y  + " i " + i);
+			System.out.println("x  " + x + "  y:  " + y  + " i " + i);
 			insert_edge(x,y,directed);
 		}  
-		   // order edges to make it easier to see what is going on
+		// order edges to make it easier to see what is going on
 		for(int i=0;i<=nvertices-1;i++)	{
 			Collections.sort(vertices.get(i).edges);
 		}  
 	}
-   
-   static void process_vertex_early(Vertex v)	{
+
+	static void process_vertex_early(Vertex v)	{
 		timer++;
 		v.entry_time = timer;
 		//     System.out.printf("entered vertex %d at time %d\n",v.key, v.entry_time);
 	}
-	
+
 	static void process_vertex_late(Vertex v)	{
 		timer++;
 		v.exit_time = timer;
@@ -64,17 +64,17 @@ class GraphStart3  {
 		else if (c == CROSS) System.out.printf("cross edge (%d,%d)\n",x.key,y.key);
 		else System.out.printf("edge (%d,%d)\n not in valid class=%d",x.key,y.key,c);	
 	}
-	
-	static void initialize_search(GraphStart3 g)	{
+
+	static void initialize_search(MyGraph g)	{
 		for(Vertex v : g.vertices)		{
 			v.processed = v.discovered = false;
 			v.parent = null;
 		}
 	}
-	
+
 	static final int TREE = 1, BACK = 2, FORWARD = 3, CROSS = 4;
 	static int timer = 0;
-	
+
 	static int edge_classification(Vertex x, Vertex y)	{
 		if (y.parent == x) return(TREE);
 		if (y.discovered && !y.processed) return(BACK);
@@ -83,18 +83,18 @@ class GraphStart3  {
 		System.out.printf("Warning: self loop (%d,%d)\n",x,y);
 		return -1;
 	}
-   
+
 	void insert_edge(int x, int y, boolean directed) 	{
 		Vertex one = vertices.get(x-1);
-      Vertex two = vertices.get(y-1);
-      one.edges.add(two);      
+		Vertex two = vertices.get(y-1);
+		one.edges.add(two);      
 		vertices.get(x-1).degree++;
 		if(!directed)
 			insert_edge(y,x,true);
 		else
 			nedges++;
 	}
-   void remove_edge(Vertex x, Vertex y)  {
+	void remove_edge(Vertex x, Vertex y)  {
 		if(x.degree<0)
 			System.out.println("Warning: no edge --" + x + ", " + y);
 		x.edges.remove(y);
@@ -110,36 +110,47 @@ class GraphStart3  {
 		}
 	} 
 
-   class Vertex implements Comparable<Vertex> {
-      int key;
-      int degree;
-      int component;
-      int color = -1;    // use mod numColors, -1 means not colored
-      boolean discovered = false;
-      boolean processed = false;
-      int entry_time = 0;
-      int exit_time = 0;
-      Vertex parent = null;
-      ArrayList<Vertex> edges = new ArrayList<Vertex>();
-   
-      public Vertex(int tkey){ 
-         key = tkey;
-      }
-      public int compareTo(Vertex other){
-         if (this.key > other.key){
-            return 1;
-         }         else if (this.key < other.key) {
-            return -1;
-         }
-         else 
-            return 0;
-         }
-      }
+	class Vertex implements Comparable<Vertex> {
+		int key;
+		int degree;
+		int component;
+		int color = -1;    // use mod numColors, -1 means not colored
+		boolean discovered = false;
+		boolean processed = false;
+		int entry_time = 0;
+		int exit_time = 0;
+		Vertex parent = null;
+		ArrayList<Vertex> edges = new ArrayList<Vertex>();
+
+		public Vertex(int tkey){ 
+			key = tkey;
+		}
+		public int compareTo(Vertex other){
+			if (this.key > other.key){
+				return 1;
+			}         else if (this.key < other.key) {
+				return -1;
+			}
+			else 
+				return 0;
+		}
+	}
 
 	Vertex unProcessedV(){	   
-      for(Vertex v:  vertices)  {
-         if (! v.processed ) return v;
-      }
-   return null;	
-   }
+		for(Vertex v:  vertices)  {
+			if (! v.processed ) return v;
+		}
+		return null;	
+	}
+
+	public static void main(String[] args){
+		MyGraph g=new MyGraph();
+		try{
+			g.readfile_graph("Bfstest1.txt");
+		}
+		catch (Exception e) {
+			System.out.println(e.getClass());
+		}
+
+	}
 }
