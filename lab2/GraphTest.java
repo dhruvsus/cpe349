b5fdaf1a67c4128e87475bc0fafa6102bce83603
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-01-18 10:15:23 -0800

    starting asginment 2

--- lab2/GraphTest.java
+++ lab2/GraphTest.java
@@ -4,7 +4,7 @@ public class GraphTest {
     public static void main(String[] args) {
         MyGraph g = new MyGraph();
         try {
-            g.readfile_graph("mytest3.txt");
+            g.readfile_graph("mytest4.txt");
             //System.out.println(g.connectCheck());
             System.out.println(g.bipartiteCheck());
         } catch (Exception e) {
