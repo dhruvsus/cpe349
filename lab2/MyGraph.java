a031f3482e0359418be377de2f65814815cd075c
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-01-18 00:49:39 -0800

    changed location of hashset declaration. todo: fix numcomp

--- lab2/MyGraph.java
+++ lab2/MyGraph.java
@@ -7,68 +7,73 @@ import java.util.LinkedList;
 import java.util.Collections;
 import java.util.HashSet;
 
-
-class MyGraph  {	
+class MyGraph {
 	ArrayList<Vertex> vertices = new ArrayList<Vertex>();
-	boolean directed; 
+	boolean directed;
 	int nvertices;
-	int nedges; 
+	int nedges;
 	int numComp;
 
-	public MyGraph(){
+	public MyGraph() {
 	}
 
-	void readfile_graph(String filename) throws FileNotFoundException  {
-		int x,y;
-		//read the input
+	void readfile_graph(String filename) throws FileNotFoundException {
+		int x, y;
+		// read the input
 		FileInputStream in = new FileInputStream(new File(filename));
 		Scanner sc = new Scanner(in);
 		int temp = sc.nextInt(); // if 1 directed; 0 undirected
 		directed = (temp == 1);
 		nvertices = sc.nextInt();
-		for (int i=0; i<=nvertices-1; i++){
-			Vertex tempv = new Vertex(i+1);   // kludge to store proper key starting at 1
+		for (int i = 0; i <= nvertices - 1; i++) {
+			Vertex tempv = new Vertex(i + 1); // kludge to store proper key starting at 1
 			vertices.add(tempv);
 		}
 
-		nedges = sc.nextInt();   // m is the number of edges in the file
+		nedges = sc.nextInt(); // m is the number of edges in the file
 		int nedgesFile = nedges;
-		for (int i=1; i<=nedgesFile ;i++)	{
-			System.out.println(i + " compare " + (i<=nedges) + " nedges " + nedges);
-			x=sc.nextInt();
-			y=sc.nextInt();
-			System.out.println("x  " + x + "  y:  " + y  + " i " + i);
-			insert_edge(x,y,directed);
-		}  
+		for (int i = 1; i <= nedgesFile; i++) {
+			System.out.println(i + " compare " + (i <= nedges) + " nedges " + nedges);
+			x = sc.nextInt();
+			y = sc.nextInt();
+			System.out.println("x  " + x + "  y:  " + y + " i " + i);
+			insert_edge(x, y, directed);
+		}
+		sc.close();
 		// order edges to make it easier to see what is going on
-		for(int i=0;i<=nvertices-1;i++)	{
+		for (int i = 0; i <= nvertices - 1; i++) {
 			Collections.sort(vertices.get(i).edges);
-		}  
+		}
 	}
 
-	static void process_vertex_early(Vertex v)	{
+	static void process_vertex_early(Vertex v) {
 		timer++;
 		v.entry_time = timer;
-		//     System.out.printf("entered vertex %d at time %d\n",v.key, v.entry_time);
+		// System.out.printf("entered vertex %d at time %d\n",v.key, v.entry_time);
 	}
 
-	static void process_vertex_late(Vertex v)	{
+	static void process_vertex_late(Vertex v) {
 		timer++;
 		v.exit_time = timer;
-		//     System.out.printf("exit vertex %d at time %d\n",v.key , v.exit_time);
+		// System.out.printf("exit vertex %d at time %d\n",v.key , v.exit_time);
 	}
 
-	static void process_edge(Vertex x,Vertex y) 	{
-		int c = edge_classification(x,y);
-		if (c == BACK) System.out.printf("back edge (%d,%d)\n",x.key,y.key);
-		else if (c == TREE) System.out.printf("tree edge (%d,%d)\n",x.key,y.key);
-		else if (c == FORWARD) System.out.printf("forward edge (%d,%d)\n",x.key,y.key);
-		else if (c == CROSS) System.out.printf("cross edge (%d,%d)\n",x.key,y.key);
-		else System.out.printf("edge (%d,%d)\n not in valid class=%d",x.key,y.key,c);	
+	static void process_edge(Vertex x, Vertex y) {
+		int c = edge_classification(x, y);
+		if (c == BACK)
+			System.out.printf("back edge (%d,%d)\n", x.key, y.key);
+		else if (c == TREE)
+			System.out.printf("tree edge (%d,%d)\n", x.key, y.key);
+		else if (c == FORWARD)
+			System.out.printf("forward edge (%d,%d)\n", x.key, y.key);
+		else if (c == CROSS)
+			System.out.printf("cross edge (%d,%d)\n", x.key, y.key);
+		else
+			System.out.printf("edge (%d,%d)\n not in valid class=%d", x.key, y.key, c);
 	}
 
-	static void initialize_search(MyGraph g)	{
-		for(Vertex v : g.vertices)		{
+	static void initialize_search(MyGraph g) {
+		for (Vertex v : g.vertices) {
 			v.processed = v.discovered = false;
 			v.parent = null;
 		}
@@ -77,46 +82,51 @@ class MyGraph  {
 	static final int TREE = 1, BACK = 2, FORWARD = 3, CROSS = 4;
 	static int timer = 0;
 
-	static int edge_classification(Vertex x, Vertex y)	{
-		if (y.parent == x) return(TREE);
-		if (y.discovered && !y.processed) return(BACK);
-		if (y.processed && (y.entry_time > x.entry_time)) return(FORWARD);
-		if (y.processed && (y.entry_time < x.entry_time)) return(CROSS);
-		System.out.printf("Warning: self loop (%d,%d)\n",x,y);
+	static int edge_classification(Vertex x, Vertex y) {
+		if (y.parent == x)
+			return (TREE);
+		if (y.discovered && !y.processed)
+			return (BACK);
+		if (y.processed && (y.entry_time > x.entry_time))
+			return (FORWARD);
+		if (y.processed && (y.entry_time < x.entry_time))
+			return (CROSS);
+		System.out.printf("Warning: self loop (%d,%d)\n", x, y);
 		return -1;
 	}
 
-	void insert_edge(int x, int y, boolean directed) 	{
-		Vertex one = vertices.get(x-1);
-		Vertex two = vertices.get(y-1);
-		one.edges.add(two);      
-		vertices.get(x-1).degree++;
-		if(!directed)
-			insert_edge(y,x,true);
+	void insert_edge(int x, int y, boolean directed) {
+		Vertex one = vertices.get(x - 1);
+		Vertex two = vertices.get(y - 1);
+		one.edges.add(two);
+		vertices.get(x - 1).degree++;
+		if (!directed)
+			insert_edge(y, x, true);
 		else
 			nedges++;
 	}
-	void remove_edge(Vertex x, Vertex y)  {
-		if(x.degree<0)
+
+	void remove_edge(Vertex x, Vertex y) {
+		if (x.degree < 0)
 			System.out.println("Warning: no edge --" + x + ", " + y);
 		x.edges.remove(y);
 		x.degree--;
-	} 
+	}
 
-	void print_graph()	{
-		for(Vertex v : vertices)	{
-			System.out.println("vertex: "  + v.key);
-			for(Vertex w : v.edges)
+	void print_graph() {
+		for (Vertex v : vertices) {
+			System.out.println("vertex: " + v.key);
+			for (Vertex w : v.edges)
 				System.out.print("  adjacency list: " + w.key);
 			System.out.println();
 		}
-	} 
+	}
 
 	class Vertex implements Comparable<Vertex> {
 		int key;
 		int degree;
 		int component;
-		int color = -1;    // use mod numColors, -1 means not colored
+		int color = -1; // use mod numColors, -1 means not colored
 		boolean discovered = false;
 		boolean processed = false;
 		int entry_time = 0;
@@ -124,69 +134,76 @@ class MyGraph  {
 		Vertex parent = null;
 		ArrayList<Vertex> edges = new ArrayList<Vertex>();
 
-		public Vertex(int tkey){ 
+		public Vertex(int tkey) {
 			key = tkey;
 		}
-		public int compareTo(Vertex other){
-			if (this.key > other.key){
+
+		public int compareTo(Vertex other) {
+			if (this.key > other.key) {
 				return 1;
-			}         else if (this.key < other.key) {
+			} else if (this.key < other.key) {
 				return -1;
-			}
-			else 
+			} else
 				return 0;
 		}
 	}
 
-	Vertex unProcessedV(){	   
-		for(Vertex v:  vertices)  {
-			if (! v.processed ) return v;
+	Vertex unProcessedV() {
+		for (Vertex v : vertices) {
+			if (!v.processed)
+				return v;
 		}
-		return null;	
+		return null;
 	}
 
-	//BFS over all the vertices
-	public ArrayList<HashSet<Integer>> connectCheck(){
-		ArrayList<HashSet<Integer>> descSets=new ArrayList<HashSet<Integer>>();
-		for(int i=0;i<nvertices;i++){
-			//hashset to store vertices of a connected component
-			HashSet<Integer> containedComp=new HashSet<Integer>();
-			Vertex v=vertices.get(i);
-			//debugging, print the initial key
-			if(v.discovered==false){
+	// BFS over all the vertices
+	public void connectCheck() {
+		// ArrayList containing all the HashSets
+		ArrayList<HashSet<Integer>> graphConnect = new ArrayList<HashSet<Integer>>();
+		// go through all unvisited vertices
+		for (int i = 0; i < nvertices; i++) {
+			if (vertices.get(i).discovered == false) {
+				// new connected component
+				// hashset for each connected component
+				HashSet<Integer> compVert = new HashSet<Integer>();
 				numComp++;
-				System.out.println("numcomp"+numComp);
-				LinkedList<Vertex> component=new LinkedList<Vertex>();
-				component.add(0,v);
-				containedComp.add(v.key);
-				//set visited to true and add to queue
-				v.discovered=true;
-				//start exploring
-
-				while(component.size()>0){
-					v = component.remove(0);
-					for(Vertex w:v.edges){
-						if(w.discovered==false){
-							w.discovered=true;
-							component.add(0,w);
-							//containedComp.add(w.key);
+				// vertex of new connected component to queue
+				LinkedList<Vertex> queue = new LinkedList<Vertex>();
+				// add to end of the queue
+				queue.add(vertices.get(i));
+				// set v to be discovered but not processed
+				vertices.get(i).discovered = true;
+				// process the vertices in the queue by popping them off the front
+				// and process them and add their neighbours if not discovered
+				// to the back of the queue
+				while (queue.size() != 0) {
+					// experiment with declaration of v
+					Vertex v = queue.poll();
+					v.processed = true;
+					compVert.add(v.key);
+					// add neighbours and set them to discovered but not processed
+					for (Vertex w : v.edges) {
+						if (w.discovered == false) {
+							w.discovered = true;
+							queue.add(w);
 						}
 					}
 				}
+				// add the connected component's hashset to arraylist end
+				graphConnect.add(compVert);
 			}
-			descSets.add(containedComp);
 		}
-		HashSet<Integer> nComp=new HashSet<Integer>(numComp);
-		descSets.add(0,nComp);
-		return descSets;
+		// finally add hashset with the number of components
+		graphConnect.add(0, new HashSet<Integer>(numComp));
+		System.out.println(graphConnect);
 	}
-	public static void main(String[] args){
-		MyGraph g=new MyGraph();
-		try{
+
+	public static void main(String[] args) {
+		MyGraph g = new MyGraph();
+		try {
 			g.readfile_graph("mytest2.txt");
-			System.out.println(g.connectCheck());
-		}
-		catch (Exception e) {
+			g.connectCheck();
+		} catch (Exception e) {
 			System.out.println(e.getClass());
 		}
 
