54577f08bb77170afbe7484c2277374e2274f5cb
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-01-23 17:51:04 -0800

    no more lab

--- lab3/ArraySearch.java
+++ lab3/ArraySearch.java
@@ -4,8 +4,11 @@ public class ArraySearch{
 		int numColums=curArray[0].length;
 		//start from top right
 		int[] position=new int[]{0,numColums-1};
+		int comp=0;
 		while(position[0]<curArray.length && position[1]>=0){
+			comp++;
 			if(curArray[position[0]][position[1]]==target){
+				System.out.println(comp);
 				return position;
 			}
 			else if(curArray[position[0]][position[1]]>target){
@@ -18,16 +21,16 @@ public class ArraySearch{
 		position[0]=-1;
 		position[1]=-1;
 		return position;
-	}/*
+	}/*	
 	public static void main(String[] args) { 
-		int mat[][] = { {10, 20, 30, 40}, 
-			{15, 25, 35, 45}, 
-			{27, 29, 37, 48}, 
-			{32, 33, 39, 50} };
+	int mat[][] = { {1, 2, 8, 9}, 
+	{3, 6, 12, 13}, 
+	{7, 10, 13, 19}, 
+	{10, 11, 28, 30} };
 
-		int[] result=search(mat,1);
-		System.out.println(result[0]+", "+result[1]);
-	} */
+	int[] result=search(mat,12);
+	System.out.println(result[0]+", "+result[1]);
+	}*/
 
 } 
 
