7b44f30a460ccdbf85829c8c472859c676fda698
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-02-14 14:41:11 -0800

    moving to t430

---
+++ lab5/RodCutter.java
@@ -0,0 +1,11 @@
+public class RodCutter{
+	private int[] table;
+	private int[] prices;
+	private int[] cuts;
+
+	public RodCutter(){}
+	public readFileIn(String filename){
+		// read in number of cases
+
+	}
+}
