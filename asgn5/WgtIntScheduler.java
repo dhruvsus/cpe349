b4e7e714b2cd5485633fcb76b19a0023847a81c6
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-02-15 09:50:18 -0800

    streams make me scream

--- asgn5/WgtIntScheduler.java
+++ asgn5/WgtIntScheduler.java
@@ -1,14 +1,26 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream;
+import java.util.stream.Collectors;
+
 public class WgtIntScheduler {
     public static int[] getOptSet(int[] stime, int[] ftime, int[] weight) {
         int[] jobs;
-        // find max ftime
-        int fMax = Arrays.stream(ftime).max().getAsInt();
+        int size = ftime.length + 1;
         // table of solutions
-        // dimensions = #jobs+1 x max_finish_time+1(since we can assume that the 0th job
+        // dimensions = #jobs+1 x jobs+1(since we can assume that the 0th job
         // starts and ends at 0)
-        int[][] table = new int[ftime.length + 1][fMax+1];
+        int[][] table = new int[size][size];
         // stime, ftime, and weights may not be sorted.
-        // implement my own damn sort
+        // sort stime, ftime, and weights according to ftime
+        List<Integer> stimeCopy = IntStream.of(stime).boxed().collect(Collectors.toList());
+        ArrayList<Integer> sortedStime = new ArrayList(stimeCopy);
+        Collections.sort(sortedStime, Comparator.comparing(s -> ftime[stimeCopy.indexOf(s)]));
+        for (Integer i : sortedStime) {
+            System.out.printf("%d\n", i);
+        }
         return jobs;
     }
 
