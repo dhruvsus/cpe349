a0f97ac4f928218a1e8966b9943e63c3fe4ee04b
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-02-14 18:50:29 -0800

    done with lab5. no touching

--- lab5/RodCutter.java
+++ lab5/RodCutter.java
@@ -7,31 +7,27 @@ import java.util.Scanner;
 public class RodCutter {
 	private static ArrayList<Integer> cuts;
 
-	static int[] rodCutting(int[] prices, int numPrices) {
-		int[] table = new int[numPrices + 1];
-		for (int i = 1; i <= numPrices; i++) {
-			int maxMoney = 0;
-			for (int j = 0; j < i; j++) {
-				maxMoney = Math.max(maxMoney, prices[j] + table[i - j - 1]);
+	static int[] rodCutting(int[] prices) {
+		int size = prices.length;
+		int[] table = new int[size];
+		// price for 0 inches
+		table[0] = prices[0];
+		for (int i = 1; i < size; i++) {
+			for (int j = 1; j <= i; j++) {
+				table[i] = Math.max(table[i], prices[j] + table[i - j]);
 			}
-			table[i] = maxMoney;
 		}
 		return table;
 	}
 
 	static int[] traceBack(int[] table, int[] prices) {
 		int size = table.length - 1;
-		int[] newPrices = new int[prices.length + 1];
-		// copy prices
-		System.arraycopy(prices, 0, newPrices, 1, prices.length);
-
 		int[] cuts = new int[size];
 		int i = size;
 		while (i > 0) {
 			int x = i;
 			while (x >= 0) {
-
-				if (table[i] - table[x] == newPrices[i - x]) {
+				if (table[i] - table[x] == prices[i - x]) {
 					cuts[i - x]++;
 					i = x;
 				}
@@ -41,26 +37,58 @@ public class RodCutter {
 		return cuts;
 	}
 
-	public static void main(String[] arg) throws FileNotFoundException{
-		// obtain number of cases and 
-		String filename="rodOptTest.txt";
-		//create a scanner object for file
-		Scanner scanner = new Scanner(new File(filename));
-		
+	public static void main(String[] arg) throws FileNotFoundException {
+		// vars
 		int[] table;
-		int arr[] = new int[] { 2, 4, 4, 5, 12, 13, 14, 15, 40, 41 };
-		int arr1[] = new int[] { 1, 4, 6, 25, 28, 31, 80, 81, 82, 83, 84, 85, 86, 88, 90, 92 };
-		int size = arr1.length;
-		table = new int[size + 1];
-		table = rodCutting(arr1, size);
-
-		for (int k = 1; k < table.length; k++) {
-			System.out.println("total for length " + k + " = " + table[k]);
+		int[] prices;
+		int[] cuts;
+		int rodLength;
+		int size;
+		String filename = "rodOptTest.txt";
+		// create a scanner object for file
+		Scanner scanner = new Scanner(new File(filename));
+		int numCases = scanner.nextInt();
+		for (int i = 0; i < numCases; i++) {
+			rodLength = scanner.nextInt();
+			size = rodLength + 1;
+			table = new int[size];
+			prices = new int[size];
+			cuts = new int[size];
+			// load in prices
+			// assuming number of prices provided equals rod length
+			// price for 0 inches hehe
+			prices[0] = 0;
+			for (int j = 1; j <= rodLength; j++) {
+				prices[j] = scanner.nextInt();
+			}
+			// obtain solution table
+			table = rodCutting(prices);
+			// traceback
+			cuts = traceBack(table, prices);
+			// Output
+			System.out.printf("Case %d:\n",i+1);
+			for (int k = 1; k < table.length; k++) {
+				System.out.printf("total for length %d     = %d\n", k, table[k]);
+			}
+			System.out.printf("Optimal rod cutting\n");
+			for (int k = 1; k < cuts.length; k++) {
+				if(cuts[k]>0){
+					System.out.printf("Number of rods of length %d      = %d\n",k,cuts[k]);
+				}
+			}
 		}
-		table = traceBack(table, arr1);
 
-		for (int k = 1; k < table.length; k++) {
-			System.out.println("number of cuts of size " + k + " = " + table[k]);
-		}
+		/*
+		 * int arr[] = new int[] { 2, 4, 4, 5, 12, 13, 14, 15, 40, 41 }; int arr1[] =
+		 * new int[] { 1, 4, 6, 25, 28, 31, 80, 81, 82, 83, 84, 85, 86, 88, 90, 92 };
+		 * table = rodCutting(arr1, size);
+		 * 
+		 * for (int k = 1; k < table.length; k++) {
+		 * System.out.println("total for length " + k + " = " + table[k]); } table =
+		 * traceBack(table, arr1);
+		 * 
+		 * for (int k = 1; k < table.length; k++) {
+		 * System.out.println("number of cuts of size " + k + " = " + table[k]); }
+		 */
 	}
 }
