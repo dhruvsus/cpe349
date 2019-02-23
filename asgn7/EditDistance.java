c502b475d5084c4017eb046f174ccd241eebbb00
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-02-22 16:37:49 -0800

    making the table complete. todo: backtrace

--- asgn7/EditDistance.java
+++ asgn7/EditDistance.java
@@ -17,30 +17,60 @@ public class EditDistance {
         for (int j = 0; j < xSize; j++) {
             table[0][j] = 2 * j;
         }
-        // print table
+        /*
+        // print table before
+        System.out.printf("table before filling\n");
         for (int i = 0; i < ySize; i++) {
             for (int j = 0; j < xSize; j++) {
                 System.out.printf("%d ", table[i][j]);
             }
             System.out.println();
         }
+        */
+
+        // fill table
+        /*
+         * recurrence relation: Ed(i,j)= Ed(i-1,j-1) if match, else
+         * min((Ed(i,j-1)+2),(Ed(i-1,j)+2))
+         */
+        for(int i=1;i<ySize;i++){
+            for(int j=1;j<xSize;j++){
+                if(y[i-1].equals(x[j-1])){
+                    table[i][j]=table[i-1][j-1];
+                }
+                else{
+                    table[i][j]= Math.min(table[i-1][j-1]+1,Math.min(table[i-1][j]+2,table[i][j-1]+2));
+                }
+            }
+        }
+        /*
+        System.out.printf("table after filling\n");
+        for (int i = 0; i < ySize; i++) {
+            for (int j = 0; j < xSize; j++) {
+                System.out.printf("%d ", table[i][j]);
+            }
+            System.out.println();
+        }
+        */
+        editDistance=table[ySize-1][xSize-1];
         return editDistance;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         // driver to test
-        String x = "AACAGTTACC";
-        String y = "TAAGGTCA";
+        //String x = "AACAGTTACC";
+        //String y = "TAAGGTCA";
+
+        // accept filename through args
+        if (args.length < 1) { return; } 
+        String filename = args[0]; 
+        // create scanner to scan in string 
+        Scanner scanner = new Scanner(new File(filename)); 
+        
+        String x = scanner.nextLine(); 
+        String y = scanner.nextLine();
         int editDistance = dynamicEditDistance(x.split(""), y.split(""));
         System.out.printf("Edit distance = %d\n", editDistance);
-        // accept filename through args
-        /* if (args.length < 1) {
-            return;
-        }
-        String filename = args[0];
-        // create scanner to scan in string
-        Scanner scanner = new Scanner(new File(filename));
-        String x = scanner.nextLine();
-        String y = scanner.nextLine(); */
+        
     }
 }
