7598555eea47bb48f005913449db77eac5fecf8a
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-03-07 21:24:46 -0800

    doing BRUTE force:)

--- finalproj/BruteForce.java
+++ finalproj/BruteForce.java
@@ -1,5 +1,54 @@
 import java.util.ArrayList;
-public class BruteForce{
+
+public class BruteForce {
     int n; // number of items
+    int[] weights;
+    int[] values;
+    int capacity;
     ArrayList<String> strings; // generated binary strings for arrangements
+
+    public BruteForce(int n, int[] value, int[] weight, int capacity) {
+        this.n = n;
+        this.values = value;
+        this.weights = weight;
+        this.capacity = capacity;
+    }
+
+    private ArrayList<String> getGrayCode(int n) {
+        ArrayList<String> grayCode = new ArrayList<String>();
+        ArrayList<String> temp = new ArrayList<String>();
+        if (n > 1) {
+            // recursive
+            temp = getGrayCode(--n);
+            // prepend a 0
+            for (int i = 0; i < temp.size(); i++) {
+                grayCode.add("0" + temp.get(i));
+            }
+            // prepend 1
+            for (int i = temp.size(); i-- > 0;) {
+                grayCode.add("1" + temp.get(i));
+            }
+            return grayCode;
+        } else {
+            // graycode for n=1 is just {0,1}, it's the base case
+            grayCode.add("0");
+            grayCode.add("1");
+            return grayCode;
+        }
+    }
+
+    public void getMaxProfit() {
+        ArrayList<String> binString = getGrayCode(n);
+        int numStrings = binString.size();
+        int maxProfit = 0;
+        String[] binStr = new String[n];
+        int[] binInt = new int[n];
+        String maxbinString;
+        for (String str : binString) {
+            binStr = str.toCharArray();
+            for (int i = 0; i < binStr.length; i++) {
+                
+            }
+        }
+    }
 }
--- finalproj/Knapsack.java
+++ finalproj/Knapsack.java
@@ -1,5 +1,31 @@
-public class Knapsack{
-    public static void main(String[] args){
-        System.out.println("lets get started");
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Knapsack {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length < 1) {
+            return;
+        }
+        String filename = args[0];
+        // scanner to scan in file
+        Scanner scanner = new Scanner(new File(filename));
+        int numItems = scanner.nextInt();
+        int[] identifier = new int[numItems]; // not really needed
+        int[] value = new int[numItems];
+        int[] weight = new int[numItems];
+        for (int i = 0; i < numItems; i++) {
+            identifier[i] = scanner.nextInt();
+            value[i] = scanner.nextInt();
+            weight[i] = scanner.nextInt();
+        }
+        int capacity = scanner.nextInt();
+        // sanity checks
+        System.out.printf("capacity = %d\nnumber of items = %d\n",capacity,numItems);
+        // done scanning file
+        // Brute Force
+        BruteForce bf=new BruteForce(numItems,value,weight,capacity);
+        bf.getMaxProfit();
+        return;
     }
 }
